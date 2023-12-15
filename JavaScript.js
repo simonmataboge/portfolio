51cd32697194dd731a904f69aaa676cf8c0cc1c6
simonmataboge <50328375+simonmataboge@users.noreply.github.com>
2023-12-15 02:22:44 -0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,93 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word)) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span - document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+    let currentWord = [currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }; 340 = i * 80);
+});
+currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+
+};
+
+changeText();
+setInterval(changeText, 3000)
+
+window.addEventListener("scroll", activeMenu);
+
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", funtion(){
+    header.classList.toggle("sticky", window.scrollY > 50)
+})
+
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".menulist");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = () => {
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+};
+
+
+
+
+
+// parallax///////////////////////////////////////
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry)=> {
+    if (entry.isIntersecting) {
+        entry.target.classList.add("show-items");
+    } else {
+        entry.target.classList.remove("show-items");
+    }
+});
+        });
+
+
+
+
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
+
+
+
